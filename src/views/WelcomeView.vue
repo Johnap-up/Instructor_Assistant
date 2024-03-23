55285a25baeae81959c0f42218759189610b7799
tukumi <tukumij@gmail.com>
2024-03-23 11:10:18 +0800

    welcome

---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,32 @@
+<script setup>
+import YaZi from '@/assets/image/YaZi.png';
+</script>
+
+<template>
+  <div class="outerBox">
+    <div class="imageBox">
+      <el-image :src="YaZi" fit="cover" style="width: 100%; height: 100%"></el-image>
+    </div>
+    <div class="rightCard">
+
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+.outerBox{
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  display: flex;
+}
+.imageBox{
+  flex: 1;
+  background-color: black;
+}
+.rightCard{
+  width: 400px;
+  background-color: white;
+}
+</style>
